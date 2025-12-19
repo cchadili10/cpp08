0657cf93f0348a61b4532b0e036ee15400153743
cchadili <chadilisafi@gmail.com>
2025-12-19 21:29:51 +0100

    ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <stack>
+#include <iostream>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+        
+    public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        MutantStack(){};
+        MutantStack(const MutantStack &other): std::stack<T>(other) {};
+        MutantStack &operator=(const MutantStack &other)
+        {
+            if (this != &other)
+                std::stack<T>::operator=(other);
+            return *this;
+        };
+
+        iterator begin() { return this->c.begin(); }
+        iterator end() { return this->c.end(); }
+
+        const_iterator begin() const { return this->c.begin(); }
+        const_iterator end() const { return this->c.end(); }
+        ~MutantStack(){};
+};
+
